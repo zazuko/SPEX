5b3cc0285f940e1a2bbb4a580eadd782b215ec73
Martin Maillard <self@martin-maillard.com>
2020-04-21 18:08:22 +0200

    Add dirty table rendering with connections

---
+++ src/components/OverviewTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="OverviewTable card">
+    <header class="card-header">
+      <h3 class="card-header-title">
+        <span class="connector" :data-id="table.id"></span>
+        {{ table.name }}
+      </h3>
+    </header>
+    <table class="card-content table">
+      <tr v-for="column in table.columns" :key="column.id">
+        <th>{{ column.name }}</th>
+        <td>{{ column.datatype }}</td>
+        <td><span class="connector" :data-id="column.id"></span></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.OverviewTable {
+  margin: 1em;
+}
+</style>
+
+<script>
+
+export default {
+  props: ['table', 'overview'],
+  data () {
+    return {}
+  }
+}
+</script>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,5 +1,72 @@
 <template>
   <div class="section">
     <h1 class="title">Overview</h1>
+
+    <div class="OverviewTables">
+      <OverviewTable v-for="table in tables" :key="table.id" :table="table" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.OverviewTables {
+  display: flex;
+}
+</style>
+
+<script>
+import { jsPlumb } from 'jsplumb'
+import OverviewTable from '@/components/OverviewTable.vue'
+
+export default {
+  components: {
+    OverviewTable
+  },
+
+  data () {
+    return {
+      tables: [
+        {
+          id: 'tiger',
+          name: 'Tiger',
+          columns: [
+            { id: 'name', name: 'Name', datatype: 'string' },
+            { id: 'friend', name: 'Friend', datatype: 'cat' }
+          ]
+        },
+        {
+          id: 'cat',
+          name: 'Cat',
+          columns: [
+            { id: 'name', name: 'Name', datatype: 'string' }
+          ]
+        }
+      ]
+    }
+  },
+
+  mounted () {
+    const relations = this.tables
+      .flatMap(table => table.columns)
+      .reduce((acc, column) => {
+        const source = document.querySelector(`[data-id="${column.id}"]`)
+        const target = document.querySelector(`[data-id="${column.datatype}"]`)
+
+        if (source && target) {
+          acc.push({ source, target })
+        }
+
+        return acc
+      }, [])
+    console.log(relations)
+
+    const plumb = jsPlumb.getInstance({
+      Container: this.$el
+    })
+
+    plumb.ready(() => {
+      relations.forEach(plumb.connect)
+    })
+  }
+}
+</script>
