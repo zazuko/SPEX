4bee6298256e08c90c0da607125f2e008aa63536
Martin Maillard <self@martin-maillard.com>
2020-04-22 15:56:30 +0200

    Remove useless navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <div id="app">
-    <b-navbar>
-      <template slot="start">
-        <b-navbar-item tag="router-link" to="/">
-          Home
-        </b-navbar-item>
-        <b-navbar-item tag="router-link" to="/overview">
-          Overview
-        </b-navbar-item>
-      </template>
-    </b-navbar>
-
     <router-view/>
   </div>
 </template>
