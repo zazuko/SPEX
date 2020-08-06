62e4b62c8c2d27baf338b22f1b18151d76b462b4
Martin Maillard <self@martin-maillard.com>
2020-08-06 16:02:23 +0200

    Add experimental SHACL editor

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Overview from '../views/Overview.vue'
+import ShaclEditor from '../views/ShaclEditor.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Overview',
     component: Overview
+  },
+  {
+    path: '/shacl',
+    name: 'ShaclEditor',
+    component: ShaclEditor
   }
 ]
 
---
+++ src/views/ShaclEditor.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="Overview">
+    <Splitpanes class="default-theme">
+      <Pane size="30">
+        <h2 class="title is-6">SHACL editor</h2>
+        <!-- <rdfjs-editor class="shacl-editor" format="text/turtle" :serialized="this.shacl" ref="shaclEditor" /> -->
+        <textarea v-model="shacl" class="shacl-editor" />
+        <b-button type="is-primary" icon-left="sync" @click="loadShacl">Update</b-button>
+      </Pane>
+      <Pane>
+        <Splitpanes horizontal>
+          <Pane>
+            <div class="OverviewPane">
+              <h2 class="title is-6">Representation</h2>
+
+              <OverviewTables :tables="tables" />
+
+              <div class="section" v-if="error">
+                <div class="message is-danger">
+                  <div class="message-body">
+                    Error loading data: {{ error }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Pane>
+        </Splitpanes>
+      </Pane>
+    </Splitpanes>
+  </div>
+</template>
+
+<style scoped>
+.Overview {
+  flex-grow: 1;
+  overflow-y: hidden;
+}
+
+.OverviewPane {
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  overflow: hidden;
+}
+
+.splitpanes .splitpanes__pane {
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.title {
+  padding-bottom: calc(0.375em - 1px);
+  padding-left: 0.75em;
+  padding-right: 0.75em;
+  padding-top: calc(0.375em - 1px);
+}
+
+.shacl-editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<script>
+import RDF from 'rdf-ext'
+import N3Parser from 'n3/lib/N3Parser'
+import { shrink } from '@zazuko/rdf-vocabularies'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+// import { NavigationFailureType } from 'vue-router/src/history/errors'
+import OverviewTables from '@/components/OverviewTables.vue'
+// import { Endpoint } from '@/endpoint'
+// import config from '@/config'
+import { tablesFromSHACL } from '@/shacl'
+
+const initialEditorContent = `
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+`
+
+export default {
+  components: {
+    OverviewTables,
+    Splitpanes,
+    Pane
+  },
+
+  data () {
+    return {
+      shacl: initialEditorContent,
+      tables: [],
+      error: null
+    }
+  },
+
+  methods: {
+    loadShacl () {
+      this.error = null
+      this.tables = []
+
+      const endpoint = {
+        shrink (uri) {
+          return shrink(uri) || uri
+        }
+      }
+
+      const parser = new N3Parser()
+      try {
+        const quads = parser.parse(this.shacl)
+        const dataset = RDF.dataset(quads)
+        this.tables = tablesFromSHACL(dataset, endpoint)
+      } catch (e) {
+        this.error = e
+      }
+    }
+  }
+}
+</script>
