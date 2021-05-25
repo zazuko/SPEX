19566add3d02fa8022dd05251cf146791dd0e305
Martin Maillard <git@martinmaillard.com>
2021-05-25 10:30:41 +0200

    Add button to close resources explorer

--- src/components/ResourcesExplorer.vue
+++ src/components/ResourcesExplorer.vue
@@ -1,40 +1,51 @@
 <template>
-  <GraphLayout
-    :nodes="resources"
-    :links="links"
-    :active-links="activeLinks"
-    :auto-zoom="false"
-    @link-enter="onLinkHover"
-    @link-out="onUnhover"
-  >
-    <template v-slot:node="{ node }">
-      <ResourceCard
-        :resource="node"
-        :active-links="activeLinks"
-        @hover-title="onHoverResource"
-        @unhover-title="onUnhover"
-        @hover-property="onHoverProperty"
-        @unhover-property="onUnhover"
-      >
-        <template v-slot:actions>
-          <b-tooltip label="Open URI in new tab">
-            <a :href="node.id" target="_blank" rel="noopener noreferrer" class="button is-light flex items-baseline gap-2">
-              <b-icon icon="external-link-alt" class="w-5" />
-            </a>
-          </b-tooltip>
-          <b-tooltip label="Hide">
-            <b-button type="is-light" icon-left="eye-slash" @click="$emit('unexplore-resource', node)" />
-          </b-tooltip>
-        </template>
-        <template v-slot:property-value="{ value }">
-          <div class="flex items-center">
-            <Term :term="value" :endpoint="endpoint" />
-            <TermExploreButton :term="value" @explore-resource="$emit('explore-resource', $event)" />
-          </div>
-        </template>
-      </ResourceCard>
-    </template>
-  </GraphLayout>
+  <div class="card h-full flex flex-col">
+    <div class="card-header has-background-light">
+      <h3 class="card-header-title">
+        Resources explorer
+      </h3>
+      <p class="card-header-icon py-0 px-1">
+        <b-button type="is-light" icon-left="times" @click="$emit('close')" title="Close" />
+      </p>
+    </div>
+    <GraphLayout
+      class="card-content p-0"
+      :nodes="resources"
+      :links="links"
+      :active-links="activeLinks"
+      :auto-zoom="false"
+      @link-enter="onLinkHover"
+      @link-out="onUnhover"
+    >
+      <template v-slot:node="{ node }">
+        <ResourceCard
+          :resource="node"
+          :active-links="activeLinks"
+          @hover-title="onHoverResource"
+          @unhover-title="onUnhover"
+          @hover-property="onHoverProperty"
+          @unhover-property="onUnhover"
+        >
+          <template v-slot:actions>
+            <b-tooltip label="Open URI in new tab">
+              <a :href="node.id" target="_blank" rel="noopener noreferrer" class="button is-light flex items-baseline gap-2">
+                <b-icon icon="external-link-alt" class="w-5" />
+              </a>
+            </b-tooltip>
+            <b-tooltip label="Hide">
+              <b-button type="is-light" icon-left="eye-slash" @click="$emit('unexplore-resource', node)" />
+            </b-tooltip>
+          </template>
+          <template v-slot:property-value="{ value }">
+            <div class="flex items-center">
+              <Term :term="value" :endpoint="endpoint" />
+              <TermExploreButton :term="value" @explore-resource="$emit('explore-resource', $event)" />
+            </div>
+          </template>
+        </ResourceCard>
+      </template>
+    </GraphLayout>
+  </div>
 </template>
 
 <script>
