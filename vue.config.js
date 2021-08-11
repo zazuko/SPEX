771c2dc7ba8000bc91e2d59a0e90a904bb407482
Martin Maillard <git@martinmaillard.com>
2021-08-11 11:08:58 +0200

    Upgrade vue-cli to 5.x

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,25 @@
+const webpack = require('webpack')
+
 module.exports = {
   transpileDependencies: [
     '@rdfjs-elements/rdf-editor',
     '@rdfjs-elements/rdf-snippet',
   ],
+
+  configureWebpack: config => {
+    // Webpack 5 doesn't provide polyfills so we need to do it manually
+    config.resolve.fallback = {
+      crypto: false,
+      stream: require.resolve('stream-browserify'),
+    }
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    )
+  },
 
   chainWebpack: config => {
     config.module
