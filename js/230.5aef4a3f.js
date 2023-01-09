6b86d48803431ffda8a57532d0ed727e0f479c27
ludovicm67 <ludovicm67@users.noreply.github.com>
2023-01-09 15:24:53 +0000

    Deploying to gh-pages from @ zazuko/SPEX@69fa68c6b7a756b6dc21fa3ebd746a8231bccd6e 🚀

--- js/230.5aef4a3f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunk_zazuko_spex"]=self["webpackChunk_zazuko_spex"]||[]).push([[230],{25230:function(t,e,a){a.r(e),a.d(e,{default:function(){return h}});var o=a(66252);const r={class:"p-4"},n=(0,o.Uk)(" You are probably looking for "),s=(0,o._)("a",{href:"https://sketch.zazuko.com",target:"_blank"},"https://sketch.zazuko.com",-1),u=[n,s];function c(t,e){return(0,o.wg)(),(0,o.iD)("div",r,u)}var k=a(83744);const p={},f=(0,k.Z)(p,[["render",c]]);var h=f}}]);
-//# sourceMappingURL=230.5aef4a3f.js.map
