078dbdbcafb0f2b79782a19b0f646f1e8eeec3d3
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2023-02-21 11:06:10 +0100

    change for npm link

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   transpileDependencies: [
@@ -22,6 +23,8 @@ module.exports = {
   },
 
   chainWebpack: config => {
+    config.resolve.symlinks(false)
+    config.resolve.alias.set('vue', path.resolve('./node_modules/vue'))
     config.module
       .rule('vue')
       .use('vue-loader')
